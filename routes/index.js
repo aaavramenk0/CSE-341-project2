28df8da03ce4fc956ac0fa50f893326cd62e3756
Oleksandr <alex.avramenko2004@gmail.com>
2023-06-28 19:19:03 -0400

    Creating user Schema and begin working on login route

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,7 @@ router.use('/', require('./swagger'));
 router.use('/recipes', require('./recipes'));
 router.use('/authors', require('./authors'));
 
+router.use('/login', require('../controllers/login'))
+router.use('/signup', require('../controllers/signup'))
+
 module.exports = router;
